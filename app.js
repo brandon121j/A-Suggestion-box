5aed555274a10bee8ceadadc96ec63fd0dd0d798
Brandon <brandon121j@gmail.com>
2021-09-23 13:53:33 -0400

    Addition of validators to suggestion model

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
@@ -10,7 +9,7 @@ var usersRouter = require('./routes/users');
 var suggestionRouter = require('./routes/suggestion-box/suggestionRouter');
 
 mongoose
-  .connect("mongodb://localhost:27017/suggestion-box", {
+  .connect("mongodb://localhost:27017/suggestion-hw", {
     useNewURLParser: true,
     useUnifiedTopology: true
   })
